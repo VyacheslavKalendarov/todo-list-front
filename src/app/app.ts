4f29a84e0bfd33d9836266c034940c7f83450c88
vyacheslav <v.b.kalendarov@mail.ru>
2026-06-07 00:11:01 +0300

    (homework-1): Add basic To-Do list functionality with task management and styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Todo } from './components/todo/todo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Todo],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('todo-list-front');
-}
+export class App {}
